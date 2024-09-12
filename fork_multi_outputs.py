74b8d330cc538691f7a3657b364dc88cd518a733
dupp <dupp@wasu.com>
2024-09-12 14:32:42 +0800

    hack: camera realtime

--- fork_multi_outputs.py
+++ fork_multi_outputs.py
@@ -63,6 +63,44 @@ face_detect_options = vision.FaceDetectorOptions(
 def make_video_frame_callback():
     infer_times = []
 
+    def stiching(src_img, out_crop):
+        head, body = out_crop, src_img
+        head_gray = cv2.cvtColor(head, cv2.COLOR_BGR2GRAY)
+        body_gray = cv2.cvtColor(body, cv2.COLOR_BGR2GRAY)
+
+        sift = cv2.SIFT_create()
+        keypoints1, descriptors1 = sift.detectAndCompute(head_gray, None)
+        keypoints2, descriptors2 = sift.detectAndCompute(body_gray, None)
+
+        # 使用FLANN匹配器进行特征匹配
+        index_params = dict(algorithm=1, trees=5)
+        search_params = dict(checks=50)
+        flann = cv2.FlannBasedMatcher(index_params, search_params)
+        matches = flann.knnMatch(descriptors1, descriptors2, k=2)
+
+        # 只保留好的匹配点
+        good_matches = []
+        for m, n in matches:
+            if m.distance < 0.7 * n.distance:
+                good_matches.append(m)
+        print(f"{len(good_matches)}")
+
+        src_pts = np.float32([keypoints1[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+        dst_pts = np.float32([keypoints2[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+
+        M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+
+        height, width, channels = body.shape
+        head_warped = cv2.warpPerspective(head, M, (width, height))
+
+        mask = np.zeros((height, width), dtype=np.uint8)
+        mask[head_warped[:, :, 0] > 0] = 255
+
+        combined = cv2.bitwise_and(body, body, mask=cv2.bitwise_not(mask))
+        combined += head_warped
+
+        return combined
+
     def frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
         # Debug #
         # print(f"w: {frame.width}, h: {frame.height}")
@@ -112,6 +150,8 @@ def make_video_frame_callback():
                 if len(infer_times) % 60 == 0:
                     logging.warning(f"{repr(e)}")
                 out_crop = driving_frame
+
+            out_crop = stiching(cv_image, out_crop)
 
         return av.VideoFrame.from_ndarray(out_crop, format="bgr24")
 
