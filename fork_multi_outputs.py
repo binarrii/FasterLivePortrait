da4d5e4e5fb20afeee87d30ce98fb464e16ee99d
dupp <dupp@wasu.com>
2024-09-13 15:26:51 +0800

    hack: camera realtime

--- fork_multi_outputs.py
+++ fork_multi_outputs.py
@@ -69,31 +69,25 @@ def make_video_frame_callback():
         head_gray = cv2.cvtColor(head, cv2.COLOR_BGR2GRAY)
         body_gray = cv2.cvtColor(body, cv2.COLOR_BGR2GRAY)
 
-        orb = cv2.ORB_create()
-        head_gpu = cv2.cuda_GpuMat()
-        body_gpu = cv2.cuda_GpuMat()
-        head_gpu.upload(head_gray)
-        body_gpu.upload(body_gray)
+        sift = cv2.SIFT_create()
+        keypoints1, descriptors1 = sift.detectAndCompute(head_gray, None)
+        keypoints2, descriptors2 = sift.detectAndCompute(body_gray, None)
 
-        keypoints1, descriptors1 = orb.detectAndComputeAsync(head_gpu, None)
-        keypoints2, descriptors2 = orb.detectAndComputeAsync(body_gpu, None)
+        index_params = dict(algorithm=1, trees=5)
+        search_params = dict(checks=50)
+        flann = cv2.FlannBasedMatcher(index_params, search_params)
+        matches = flann.knnMatch(descriptors1, descriptors2, k=2)
 
-        descriptors1 = descriptors1.download()
-        descriptors2 = descriptors2.download()
-
-        bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-        matches = bf.match(descriptors1, descriptors2)
-
-        good_matches = sorted(matches, key=lambda x: x.distance)[:10]
+        good_matches = []
+        for m, n in matches:
+            if m.distance < 0.7 * n.distance:
+                good_matches.append(m)
+        print(f"{len(good_matches)}")
 
         src_pts = np.float32([keypoints1[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
         dst_pts = np.float32([keypoints2[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
 
-        if len(good_matches) > 4:
-            M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
-            print("Homography matrix calculated successfully.")
-        else:
-            print("Not enough matches found - {}/{}".format(len(good_matches), 4))
+        M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
 
         height, width, channels = body.shape
         head_warped = cv2.warpPerspective(head, M, (width, height))
---
+++ layout.py
@@ -0,0 +1,4 @@
+import streamlit as st
+from streamlit_webrtc import WebRtcMode, webrtc_streamer
+
+st.set_page_config(page_title="Portrait", layout="wide")
