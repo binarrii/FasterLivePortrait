efd5d3d1359e92baf495427e9b6d967e3d977735
dupp <dupp@wasu.com>
2024-09-18 13:20:13 +0800

    hack: camera realtime

--- server.py
+++ server.py
@@ -1,30 +1,25 @@
 import os
 
-from starlette.responses import FileResponse
-from starlette.websockets import WebSocketDisconnect
-
 os.environ["NO_ALBUMENTATIONS_UPDATE"] = "1"
 os.environ["GLOG_v"] = "0"
 os.environ["GLOG_logtostderr"] = "0"
 os.environ["GLOG_minloglevel"] = "2"
 
-import av
+from starlette.responses import FileResponse
+from starlette.websockets import WebSocketDisconnect
 
-av.logging.set_level(av.logging.FATAL)
-av.logging.set_libav_level(av.logging.FATAL)
-
-import io
+import asyncio
 import logging
 import time
 import cv2
+import queue
 import signal
 import traceback
 
 import torch
-import uvicorn
-from PIL import Image
-from fastapi import FastAPI, WebSocket
 import numpy as np
+import uvicorn
+from fastapi import FastAPI, WebSocket, BackgroundTasks
 import mediapipe as mp
 from mediapipe.tasks.python import BaseOptions as mpBaseOptions
 from mediapipe.tasks.python import vision
@@ -40,72 +35,96 @@ if torch.cuda.is_available():
 
 infer_cfg = OmegaConf.load(conf_file)
 infer_cfg.infer_params.flag_pasteback = False
-pipe = FasterLivePortraitPipeline(cfg=infer_cfg)
-pipe.prepare_source("aijia.png", realtime=True)
-
-mp_base_options = mpBaseOptions(model_asset_path=face_detect_model)
-face_detect_options = vision.FaceDetectorOptions(
-    base_options=mp_base_options,
-    min_detection_confidence=0.5,
-    min_suppression_threshold=0.3
-)
-
-infer_times = []
 
 
-def hand_frame(frame: av.VideoFrame) -> av.VideoFrame:
-    # Debug #
-    # print(f"w: {frame.width}, h: {frame.height}")
-    # print(f"format: {frame.format}")
-    # print(f"time_base: {frame.time_base}, time: {frame.time}")
-    # print(f"pts: {frame.pts}")
-    # print(f"dts: {frame.dts}")
-    # print(f"colorspace: {frame.colorspace}")
-    # print(f"color_range: {frame.color_range}")
-    # print(f"pict_type: {frame.pict_type}")
-    # print(f"planes: {len(frame.planes)}")
-    # print(f"is_corrupt: {frame.is_corrupt}")
-    # Debug #
+class VideoFramePipeline(FasterLivePortraitPipeline):
 
-    driving_frame = frame.to_ndarray(format="bgr24")
-    print(f"driving_frame: {driving_frame.shape}")
-    # noinspection PyBroadException
-    try:
-        if not pipe.src_imgs or len(pipe.src_imgs) <= 0:
-            raise Exception("src image is empty")
-        if not pipe.src_infos or len(pipe.src_infos) <= 0:
-            raise Exception("src info is empty")
+    def __init__(self, cfg, **kwargs):
+        super().__init__(cfg, **kwargs)
+        self.prepare_source("aijia.png", realtime=True)
+        mp_base_options = mpBaseOptions(model_asset_path=face_detect_model)
+        self.face_detect_options = vision.FaceDetectorOptions(
+            base_options=mp_base_options,
+            min_detection_confidence=0.5,
+            min_suppression_threshold=0.3
+        )
+        self.infer_times = []
 
-        t0 = time.time()
+    def handle_frame(self, frame: bytes) -> bytes:
+        # noinspection PyBroadException
         try:
-            dri_crop, out_crop, out_org = pipe.run(driving_frame, pipe.src_imgs[0], pipe.src_infos[0],
-                                                   realtime=True)
-            out_crop = cv2.cvtColor(out_crop, cv2.COLOR_RGB2BGR)
-            # out_crop = stiching(cv_image, out_crop)
-            if len(infer_times) % 15 == 0:
-                frame_rgb = cv2.cvtColor(driving_frame, cv2.COLOR_BGR2RGB)
-                mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame_rgb)
-                faces = vision.FaceDetector.create_from_options(face_detect_options).detect(mp_image)
-                # noinspection PyUnresolvedReferences
-                if faces is None or len(faces.detections) <= 0:
-                    pipe.src_lmk_pre = None
-                    logging.warning("No face detected")
-                    raise Exception("No face detected")
-        finally:
-            if len(infer_times) > 7200:
-                infer_times.pop(0)
-            infer_times.append(time.time() - t0)
+            if not self.src_imgs or len(self.src_imgs) <= 0:
+                raise Exception("src image is empty")
+            if not self.src_infos or len(self.src_infos) <= 0:
+                raise Exception("src info is empty")
 
-        if len(infer_times) % 60 == 0:
-            print(f"inference median time: {np.median(infer_times) * 1000} ms/frame, "
-                  f"mean time: {np.mean(infer_times) * 1000} ms/frame")
-    except Exception as e:
-        if len(infer_times) % 60 == 0:
-            logging.warning(f"{repr(e)}")
-        out_crop = driving_frame
+            t0 = time.time()
+            try:
+                driving_frame = cv2.imdecode(np.frombuffer(frame, np.uint8), cv2.IMREAD_COLOR)
+                dri_crop, out_crop, out_org = self.run(driving_frame,
+                                                       self.src_imgs[0],
+                                                       self.src_infos[0],
+                                                       realtime=True)
+                out_crop = cv2.cvtColor(out_crop, cv2.COLOR_BGR2RGB)
+                # out_crop = stiching(cv_image, out_crop)
+                if len(self.infer_times) % 15 == 0:
+                    frame_rgb = cv2.cvtColor(driving_frame, cv2.COLOR_BGR2RGB)
+                    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame_rgb)
+                    detector = vision.FaceDetector.create_from_options(self.face_detect_options)
+                    faces = detector.detect(mp_image)
+                    # noinspection PyUnresolvedReferences
+                    if faces is None or len(faces.detections) <= 0:
+                        self.src_lmk_pre = None
+                        logging.warning("No face detected")
+                        raise Exception("No face detected")
+            finally:
+                if len(self.infer_times) > 7200:
+                    self.infer_times.pop(0)
+                self.infer_times.append(time.time() - t0)
 
-    return av.VideoFrame.from_ndarray(out_crop, format="bgr24")
+            if len(self.infer_times) % 60 == 0:
+                print(f"inference median time: {np.median(self.infer_times) * 1000} ms/frame, "
+                      f"mean time: {np.mean(self.infer_times) * 1000} ms/frame")
+        except Exception as e:
+            if len(self.infer_times) % 60 == 0:
+                logging.warning(f"{repr(e)}")
+            out_crop = driving_frame
 
+        is_success, buffer = cv2.imencode(".png", out_crop)
+        return buffer.tobytes()
+
+
+pool: queue.Queue[VideoFramePipeline] = queue.Queue(6)
+for i in range(pool.maxsize):
+    pool.put_nowait(VideoFramePipeline(cfg=infer_cfg))
+
+terminate = False
+
+
+def signal_handler(sig, frame):
+    global terminate
+    terminate = True
+
+
+signal.signal(signal.SIGINT, signal_handler)
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: dict[str, WebSocket] = {}
+
+    async def connect(self, client_id: str, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections[client_id] = websocket
+
+    def disconnect(self, client_id: str):
+        del self.active_connections[client_id]
+
+    async def send_bytes(self, client_id: str, message: bytes):
+        await self.active_connections[client_id].send_bytes(message)
+
+
+connection_manager = ConnectionManager()
 
 app = FastAPI()
 
@@ -116,44 +135,34 @@ async def index():
 
 
 @app.websocket("/ws")
-async def ws(websocket: WebSocket):
-    await websocket.accept()
-    while True:
+async def ws(websocket: WebSocket, client_id: str, tasks: BackgroundTasks):
+    async def handle_ws_message(client: str, message: bytes, pipeline: VideoFramePipeline):
+        print(f"bytes received {len(message)}")
         try:
             t0 = time.time()
+            frame = pipeline.handle_frame(message)
+            print(f"time taken: {(time.time() - t0) * 1000}ms")
 
-            rcv_bytes = await websocket.receive_bytes()
-            print(f"bytes received {len(rcv_bytes)}")
-            if len(rcv_bytes) <= 0:
-                continue
-            frame = av.VideoFrame.from_image(Image.open(io.BytesIO(rcv_bytes)))
-            frame = hand_frame(frame)
-
-            codec = av.CodecContext.create('vp9', 'w')
-            codec.width = 512
-            codec.height = 512
-            codec.pix_fmt = 'yuv420p'
-            codec.options = {'preset': 'fast'}
-
-            _ = codec.encode(frame)
-            packets = codec.encode(None)
-            if len(packets) == 0:
-                print("no packets, frame encode failed")
-                continue
-
-            for packet in packets:
-                snd_bytes = bytes(packet)
-                await websocket.send_bytes(snd_bytes)
-                print(f"bytes sent {len(snd_bytes)}")
-
-            time_taken = time.time() - t0
-            print(f"time taken: {time_taken * 1000} ms")
+            await connection_manager.send_bytes(client, frame)
+            print(f"bytes sent {len(frame)}")
         except WebSocketDisconnect:
             print("WebSocket disconnected")
-        except EOFError:
-            print("EOF")
-        except Exception as e:
-            print(f"{repr(e)}")
+            connection_manager.disconnect(client)
+        except:
+            traceback.print_stack()
+
+    try:
+        await connection_manager.connect(client_id, websocket)
+        pipeline = pool.get()
+        global terminate
+        while not terminate:
+            message = await websocket.receive_bytes()
+            asyncio.ensure_future(handle_ws_message(client_id, message, pipeline))
+    except WebSocketDisconnect:
+        print("WebSocket disconnected")
+        connection_manager.disconnect(client_id)
+    finally:
+        pool.put(pipeline)
 
 
 if __name__ == "__main__":
